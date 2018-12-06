2cdfc6d636e1793b13de2d42e713315c0edd816c
Banu Kantarcioglu <Banu.Kantarcioglu@gmail.com>
2018-12-06 23:55:26 +0300

    documenttype model tests

--- test/models/document_type_test.rb
+++ test/models/document_type_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class DocumentTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save without description" do
+    @dt = DocumentType.new
+    assert_not @dt.save, "saved document type without description"
+  end
+
+  test "creates default value with the model " do
+    @dt = DocumentType.new
+    assert_equal true, @dt.active , "default at model"
+  end
+
+  test "should default active to true " do
+    @dt = DocumentType.new
+    @dt.description = "abc"
+    @dt.save
+    assert_equal true, @dt.active , "saved document type without active"
+  end
 end
