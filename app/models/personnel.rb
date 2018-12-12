17fe8a5e8973d7c3f2da0de54f83ff148f39db17
Banu Kantarcioglu <Banu.Kantarcioglu@gmail.com>
2018-12-12 14:11:51 +0300

    add personnel validations and model tests

--- app/models/personnel.rb
+++ app/models/personnel.rb
@@ -1,4 +1,8 @@
 class Personnel < ApplicationRecord
   has_many :documents
   has_many :document_types, through: :documents
+
+  validates :name, presence: true , uniqueness: true
+  validates :pno, numericality:  {only_integer: true}, allow_blank: true #TODO allow only positive
+  validates :jobdescription, length: {in:3..100}, allow_blank: true
 end
--- test/models/personnel_test.rb
+++ test/models/personnel_test.rb
@@ -4,4 +4,47 @@ class PersonnelTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  #  validates :name, presence: true, uniqueness: true
+  #  validates :pno, numericality:  {only_integer: true}, allow_blank: true
+  #  validates :jobdescription, length: {in:3..100}, allow_blank: true
+
+  test "personnel should have name" do
+    personnel = Personnel.new
+    assert_not personnel.save, " saved personnel without a name"
+  end
+
+  test "personnel name should be unique " do
+    p1 = personnels(:dummy)
+    p1.save
+    p2 = Personnel.new
+    p2.name = p1.name
+    assert_not p2.save, "saved personnel with same name"
+  end
+
+  test "pno should be only_integer" do
+    #p = Personnel.new
+    p = personnels(:dummy)
+    p.pno = "abc"
+    assert_not p.save, "saved pno in string"
+    p.pno = 1.2
+    assert_not p.save, "saved pno in double"
+    #p.pno = -1
+    #assert_not p.save, "saved pno in negative"
+  end
+
+  test "jobdescription should be at least 3 max 100 chars" do
+    #p = Personnel.new
+    p = personnels(:dummy)
+    p.jobdescription ="a"
+    assert_not p.save, "saved jobdescription 1 chars"
+    p.jobdescription ="a" * 101
+    assert_not p.save, "saved jobdescription 101 chars"
+    p.jobdescription ="abcd"
+    assert p.save, "saved jobdescription 4 chars"
+  end
+
+
+
 end
