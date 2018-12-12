637dd725a073d3b0a412a67db0dbbd13b4e13fb7
Banu Kantarcioglu <Banu.Kantarcioglu@gmail.com>
2018-12-12 12:38:14 +0300

    add faker and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do |n|
+  name  = Faker::Name.name
+  pno = Faker::Number.number(5)
+  jobdescription = Faker::Job.title
+  status = Faker::Boolean.boolean(0.8)
+  p = Personnel.create!(name:  name,
+               pno:   pno ,
+               jobdescription: jobdescription,
+               status: status)
+  numofdocuments = Faker::Number.between(1,4)
+  numofdocuments.times do |k|
+    d = Document.new
+    d.personnel = p
+    d.document_type = DocumentType.find(Faker::Number.between(1,4))
+    #dname = documenttype[Faker::Number.between(1,4)]
+    startdate = Faker::Date.between(1.years.ago,Date.today)
+    d.startdate = startdate
+    d.enddate = Faker::Date.between(startdate,startdate + 365.days)
+    d.status = 1
+    d.save!
+  end
+
+end
